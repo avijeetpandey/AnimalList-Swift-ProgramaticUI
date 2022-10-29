5d22a2f56ce064b711d0d229c3a8cddbcb6c968a
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-29 14:52:36 +0530

    Add : Animal Listworking app

--- AnimalList/ViewController.swift
+++ AnimalList/ViewController.swift
@@ -8,12 +8,137 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    let animals: [String] = ["Lion",
+                             "Tiger",
+                             "Horse",
+                             "Duck",
+                             "Elephant",
+                             "Dog",
+                             "Platypus",
+                             "Swan",
+                             "Bull",
+                             "Ox",
+                             "Humans"]
+    
+    // MARK: - Views
+    private lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.delegate = self
+        view.dataSource = self
+        view.register(AnimalCell.self, forCellReuseIdentifier: "AnimalCell")
+        return view
+    }()
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        view.backgroundColor = .white
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-
-
+    
 }
+
+// MARK: - TableView Delegates
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell") as! AnimalCell
+        cell.bind(to: animals[indexPath.item])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+}
+
+
+// MARK: - AnimalCell
+class AnimalCell: UITableViewCell {
+    
+    // MARK: - Views
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 26)
+        label.textColor = .red
+        return label
+    }()
+    
+    // MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    func bind(to name: String) {
+        nameLabel.text = name
+    }
+    
+    // MARK: - SetupUI
+    func setupUI() {
+        contentView.addSubview(nameLabel)
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ README.md
@@ -0,0 +1,3 @@
+## AnimalList :tada: :rocket:
+This is a simple repo to demonstrate , how to create UI's programatically 
+using swift .
